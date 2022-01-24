d538c67212c55b9f11a44be4635eaf575363249b
malikertgrl <79869051+malikertgrl@users.noreply.github.com>
2022-01-24 11:04:29 +0300

    logo ımage configured

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,20 +1,18 @@
 // In App.js in a new project
 
 import React from 'react';
-import { NavigationContainer, DarkTheme, DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from '../screens/LoginScreen';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import TabNavigator from "./TabNavigator"
-import { useSelector } from 'react-redux';
 
 const Stack = createStackNavigator()
 
 
 function RootNavigation() {
-    const { isDarkmode } = useSelector(state => state.SystemReducer)
 
     return (
-        <NavigationContainer theme={isDarkmode ? DarkTheme : DefaultTheme} >
+        <NavigationContainer  >
             <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="LoginScreen" component={LoginScreen} />
                 <Stack.Screen name="TabNavigator" component={TabNavigator} />
